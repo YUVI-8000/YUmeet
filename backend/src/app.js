59cb214533e75fefa739a347c3fe7ac8fd7aa21f
YUVI-8000 <106536884+YUVI-8000@users.noreply.github.com>
2024-10-20 22:16:39 +0530

    sm

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,10 +15,10 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port", (process.env.PORT || 8000));
-app.use(cors());
+// app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({limit: "40kb",extended: true}));
-const cors = require('cors');
+
 app.use(cors({
   origin: '*', // or '*' for all origins
 }));
